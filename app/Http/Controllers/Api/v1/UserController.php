3c249f01b8c30a8c5c18c028819417dadcd14d98
Rafael Santoni <rafasantoni.dev@gmail.com>
2024-04-29 21:40:54 -0300

    :construction: feat: implementado método para inserir novo usuário

--- app/Http/Controllers/Api/v1/UserController.php
+++ app/Http/Controllers/Api/v1/UserController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\Api\v1\UserResource;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -22,7 +23,28 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'username' => 'nullable|string|min:4|max:50',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|min:6|max:50',
+            'type' => 'required|string|max:1|in:' . implode(',', array_keys(User::$tipos_usuarios)),
+            'status' => 'required|numeric|max:1',
+        ]);
+
+        if($validator->fails()) {
+           return response()->json(['error' => [$validator->errors()]], 422);
+        }
+
+        $created = User::create($validator->validated());
+
+        if($created) {
+            return response()->json([
+               'message' => 'User created',
+               'data' => new UserResource($created),
+            ], 201);
+        }
+
+        return response()->json(['error' => 'User not created'], 400);
     }
 
     /**
