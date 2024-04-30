7086c5b2c1be79eb640afdc554ef861cf9b5b478
Angélica <angelica_shigematsu@outlook.com>
2024-04-29 22:05:25 -0300

    feat: add route of register and login

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::post('/register', [User::class], 'create');
+Route::post('/login', [LoginController::class], 'auth');
+Route::get('/', [User::class], 'createcsrf_token');
+
